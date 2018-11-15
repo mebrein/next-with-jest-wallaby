f73173c47814cb154556042b8ace6ff45950edbb
Merijn van Mourik <mmourik@gmail.com>
2018-11-15 12:31:20 +0100

    latest config, working with wallaby

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,10 @@
 module.exports = function(wallaby) {
   return {
-    files: ['pages/**/*.js?(x)', 'jest.setup.js', 'jest.config.js'],
+    files: [
+      { pattern: 'pages/**/*.js?(x)', instrument: true},
+      { pattern: 'jest.setup.js', instrument: false },
+      { pattern: 'jest.config.js', instrument: false }
+    ],
     tests: ['**/*.test.js?(x)', '!node_modules/**/*.test.js?(x)'],
 
     env: {
@@ -14,6 +18,6 @@ module.exports = function(wallaby) {
 
     testFramework: 'jest',
 
-    debug: true
+    debug: false
   }
 }
