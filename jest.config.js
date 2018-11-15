b32b413c70bc81667f9bf668a5ff546337395231
Merijn van Mourik <mmourik@gmail.com>
2018-11-15 12:31:20 +0100

    latest config, working with wallaby

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    "**/*.test.js?(x)" ,
+    "!<rootDir>/node_modules/",
+    "!<rootDir>/path/to/dir/"
+  ]
 }
